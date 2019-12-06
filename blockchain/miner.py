fd5d679865188efc069e189541f10da6b828b8c1
kingmoc <joe.c.a.garcia@gmail.com>
2019-12-06 10:23:33 -0600

    Mined Lambda Coin Successfully

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -25,6 +25,11 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    last_proof_string = f"{last_proof}".encode()
+    last_proof_hash = hashlib.sha256(last_proof_string).hexdigest()
+
+    while valid_proof(last_proof_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +45,10 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
@@ -65,7 +73,14 @@ if __name__ == '__main__':
     while True:
         # Get the last proof from the server
         r = requests.get(url=node + "/last_proof")
-        data = r.json()
+        try:
+            data = r.json()
+        except ValueError:
+            print("Error:  Non-json response")
+            print("Response returned:")
+            print(r)
+            break
+        
         new_proof = proof_of_work(data.get('proof'))
 
         post_data = {"proof": new_proof,
